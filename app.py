3c9ae2a286ca9d7d1cdabf00d77fe2715907a85a
Dushyantsingh98 <50130278+Dushyantsingh98@users.noreply.github.com>
2024-04-12 11:20:42 -0400

    Update app.py

--- app.py
+++ app.py
@@ -41,5 +41,5 @@ def predict_datapoint():
         return render_template('index.html')
 
 
-if __name__=="main_":
-    app.run(host="0.0.0.0", port=5000)
+if __name__=="__main__":
+    app.run(host="0.0.0.0", port=5012)
